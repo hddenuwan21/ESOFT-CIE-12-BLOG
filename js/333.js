3cbd63e21c9ae285192b58cd5d61838c9593128b
HD DENUWAN <159047613+hddenuwan21@users.noreply.github.com>
2024-02-12 04:21:38 +0530

    Add files via upload

---
+++ js/333.js
@@ -0,0 +1,279 @@
+const categoryTitle = document.querySelectorAll('.category-title');
+const allCategoryPosts = document.querySelectorAll('.all');
+
+for(let i = 0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
+}
+
+function filterPosts(item){
+    changeActivePosition(item);
+    for(let i = 0; i < allCategoryPosts.length; i++){
+        if(allCategoryPosts[i].classList.contains(item.attributes.id.value)){
+            allCategoryPosts[i].style.display = "block";
+        } else {
+            allCategoryPosts[i].style.display = "none";
+        }
+    }
+}
+
+function changeActivePosition(activeItem){
+    for(let i = 0; i < categoryTitle.length; i++){
+        categoryTitle[i].classList.remove('act');
+    }
+    activeItem.classList.add('act');
+};
+
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars1 = document.querySelectorAll(".stars1 i");
+
+// Loop through the "stars" NodeList
+stars1.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars1.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars2 = document.querySelectorAll(".stars2 i");
+
+// Loop through the "stars" NodeList
+stars2.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars2.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars3 = document.querySelectorAll(".stars3 i");
+
+// Loop through the "stars" NodeList
+stars3.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars3.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars4 = document.querySelectorAll(".stars4 i");
+
+// Loop through the "stars" NodeList
+stars4.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars4.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars5 = document.querySelectorAll(".stars5 i");
+
+// Loop through the "stars" NodeList
+stars5.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars5.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars6 = document.querySelectorAll(".stars6 i");
+
+// Loop through the "stars" NodeList
+stars6.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars6.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars7 = document.querySelectorAll(".stars7 i");
+
+// Loop through the "stars" NodeList
+stars7.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars7.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars8 = document.querySelectorAll(".stars8 i");
+
+// Loop through the "stars" NodeList
+stars8.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars8.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars9 = document.querySelectorAll(".stars9 i");
+
+// Loop through the "stars" NodeList
+stars9.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars9.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars10 = document.querySelectorAll(".stars10 i");
+
+// Loop through the "stars" NodeList
+stars10.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars10.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars11 = document.querySelectorAll(".stars11 i");
+
+// Loop through the "stars" NodeList
+stars11.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars11.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars12 = document.querySelectorAll(".stars12 i");
+
+// Loop through the "stars" NodeList
+stars12.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars12.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars13 = document.querySelectorAll(".stars13 i");
+
+// Loop through the "stars" NodeList
+stars13.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars13.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars14 = document.querySelectorAll(".stars14 i");
+
+// Loop through the "stars" NodeList
+stars14.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars14.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars15 = document.querySelectorAll(".stars15 i");
+
+// Loop through the "stars" NodeList
+stars15.forEach((star, index1) => {
+  // Add an event listener that runs a function when the "click" event is triggered
+  star.addEventListener("click", () => {
+    // Loop through the "stars" NodeList Again
+    stars15.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
+      index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+    });
+  });
+});
+
+
+
+
+        var content = document.getElementsByTagName('body')[0];
+        var darkMode = document.getElementById('dark-change');
+        darkMode.addEventListener('click', function(){
+            darkMode.classList.toggle('active');
+            content.classList.toggle('night');
+        })
+    
---
+++ js/min.js
@@ -0,0 +1,114 @@
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .card");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars1 = document.querySelectorAll(".stars1 i");
+
+ // Loop through the "stars" NodeList
+ stars1.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars1.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+
+ 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars2 = document.querySelectorAll(".stars2 i");
+
+ // Loop through the "stars" NodeList
+ stars2.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars2.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+ 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars3 = document.querySelectorAll(".stars3 i");
+
+ // Loop through the "stars" NodeList
+ stars3.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars3.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+ 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars4 = document.querySelectorAll(".stars4 i");
+
+ // Loop through the "stars" NodeList
+ stars4.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars4.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+ 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars5 = document.querySelectorAll(".stars5 i");
+
+ // Loop through the "stars" NodeList
+ stars5.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars5.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
+ 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars6 = document.querySelectorAll(".stars6 i");
+
+ // Loop through the "stars" NodeList
+ stars6.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars6.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
